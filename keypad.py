bd23f16a021393fb69e60242b9964f3fd80dd786
Kai Hong <ricedev10@gmail.com>
2025-11-10 12:48:44 -0500

    update psuedocode, add buttons pressed and updated to a 4 pin code
    Not fully functional. Also added the library for the LCD Display.

--- keypad.py
+++ keypad.py
@@ -1,8 +1,11 @@
-from machine import Pin
+from machine import I2C, Pin
+from library.pico_i2c_lcd import I2cLcd
 from utime import sleep
+from enum import Enum
 
 # Configuration
-keycode = [0, 0, 0, 0]  # user *must* press the corresponding buttons in this order
+HOLD_DURATION = 2.0 # if user holds the button for 2+ seconds, they can change code
+keycode = [0, 0, 0, 0]  # user *must* press the corresponding buttons in this order, Note: user can change this
 
 # Buttons
 keypad_buttons = [  # replace [__] with the pin numbers
@@ -11,7 +14,6 @@ keypad_buttons = [  # replace [__] with the pin numbers
     Pin(02, Pin.IN, Pin.PULL_UP),
     Pin(03, Pin.IN, Pin.PULL_UP),
 ]
-input_button = Pin(05, Pin.IN, Pin.PULL_UP) # this button is for entering/changing keypad code
 
 # LEDs -- make sure each led corresponds to the buttons above
 keypad_LEDs = [
@@ -21,22 +23,63 @@ keypad_LEDs = [
     Pin(03, Pin.OUT),
 ]
 
+# LCD Display
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000) #Data pin is pin 0, clock pin is pin 1
+I2C_ADDR = i2c.scan()[0]
+lcd = I2cLcd(i2c, I2C_ADDR, 2, 16) 
+
 # user input variables
 current_keycode = []
+current_inputs = 0 # for keeping track of syncing the state while "sleeping"
+
+# LCD display helper functions
+def write_to_display(line1: str, line2: str | None):
+    lcd.clear()
+    lcd.putstr(line1)
+    if line2:
+        lcd.move_to(0, 1)
+        lcd.putstr(line2)
+def clear_display():
+    lcd.clear()
+
+# keycode helper functions
+def is_correct_keycode() -> bool:
+    return current_keycode == keycode
+
+def reset_keycode() -> None:
+    current_keycode = []
+
 
 # Event functions
-def on_button_press(id):
+def on_entering_keycode():
+    if is_correct_keycode():
+        # TODO: open drawer
+        pass
+    else:
+        # display incorrect
+        input_now = current_inputs
+        write_to_display(
+            "Incorrect", "Enter code again"
+        )
+        sleep(2.0)
+        if input_now == current_inputs:
+            clear_display()
+
+def on_button_pressed(id: int):
     # on pressing button...
         # 1. Keep track of user inputted code
         # 2. Light up the led
     current_keycode.append(id)
     keypad_LEDs[id].on()
 
-
-# Attach interrupt to trigger on a falling edge (button press)
+# Connect button functions to event
 for i in range(0, len(keypad_buttons) - 1):
     button = keypad_buttons[i]
-    button.irq(trigger=Pin.IRQ_RISING, handler=on_button_press)
+    def on_pressed(_pin: Pin):
+        on_button_pressed(i)
+    button.irq(trigger=Pin.IRQ_RISING, handler=on_pressed)
+
+
 
 # Main loop can now do other things
 while True:
--- psuedocode.md
+++ psuedocode.md
@@ -1,14 +1,15 @@
 # Goal
 
-1. To create a 4-digit padlock
+1. To create a 4-digit padlock keycode
 2. When pressing one of 4 buttons, a corresponding LED lights up to indicate it was pressed
 3. LCD Display (for display "Incorrect/Correct" and other information)
-4. After pressing 4 digits, program automatically detects if it is correct or not
+4. After pressing 4 digits, program automatically detects if inputted keycode is correct or not
 5. LCD display will show:
-    a. Incorrect: Enter code again
-    b. Correct: Automatically opens drawer and gives options to press button A for changing keycode (see Setting & Resetting the Pin below) & button B for closing drawer 
+   a. Incorrect: Enter keycode again
+   b. Correct: Automatically opens drawer and gives options to press button A for changing keycode (see [Setting & Resetting the Pin](Setting & Resetting the Pin below)) & button B for closing drawer
 
 # Setting & Resetting the Pin
 
-1. After successfully entering the pin, you can press the 5th button to "open" OR you can _hold_ the button to reset the pin (instructions on LCD Display)
-2. After _holding_ the 5th button, you type in a code, then press the same 5th button again to finalize the new code (hopefully you don't forget it...)
+1. After entering the correct pin, the drawer opens and the following occurs:
+2. LCD Display gives option to press button A for closing drawer (This will "reset" the circuit - must enter keycode again) OR...
+3. LCD Display gives option to press button B for changing keycode this will start the process of changing the keycode. Once B is pressed, you enter 4 digits, and to confirm you press button B once more. You can now close drawer or reset code again (goes back to step 1).
